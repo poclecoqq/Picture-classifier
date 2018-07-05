e5052308ae98fea0d2b47d2c0fccf81a60d5db53
Pierre-Olivier Cote <pierre-olivierb.cote@bnc.ca>
2018-07-05 11:20:31 -0400

    changed functionnality. Program wil not prompt sizes, will use sizes.txt

--- fileReader.sh
+++ fileReader.sh
@@ -2,5 +2,5 @@
 # took from stack overflow: https://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "Text read from file: $line"
+    echo "$line"
 done < "$1"
