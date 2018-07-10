4769c14666bd6309bfbb0ef46640d4701d7c58f7
Pierre-Olivier Cote <pierre-olivierb.cote@bnc.ca>
2018-07-09 21:45:30 -0400

    V1 - program sepaate pictures into differante folders

--- pictureSeparator.sh
+++ pictureSeparator.sh
@@ -2,14 +2,15 @@
 
 
 scriptRelativePath=`dirname $0`
-echo $scriptRelativePath
+
 
 files=`find . -maxdepth 1 -name "*.jpg" -o -name "*.png" -o -name "*.bmp" -o -name "*.gif"`
 
 #for each picture in repository
-for file in $files; do #TODO: add .png and other files extensions
+for file in $files; do
   size=$(python "$scriptRelativePath"/pictureSizeParser.py `file $file`)
-  echo $size
+  mkdir -p "PS_"$size""
+  cp $file "PS_"$size""
 done
 
 #TODO:
