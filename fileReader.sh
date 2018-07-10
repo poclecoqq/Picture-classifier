6a68e0a150451485014779f58c2c23c426f16002
Pierre-Olivier Cote <pierre-olivierb.cote@bnc.ca>
2018-07-09 20:53:02 -0400

    get size of all picture in current repository

--- fileReader.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# took from stack overflow: https://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable
-
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "$line"
-done < "$1"
--- pictureSeparator.sh
+++ pictureSeparator.sh
@@ -8,22 +8,10 @@ files=`find . -maxdepth 1 -name "*.jpg" -o -name "*.png" -o -name "*.bmp" -o -na
 
 #for each picture in repository
 for file in $files; do #TODO: add .png and other files extensions
-  file $file
+  size=$(python "$scriptRelativePath"/pictureSizeParser.py `file $file`)
+  echo $size
 done
 
-
-#############################################################
-#read from file and parse to get picture sizes
-unparsedsizes=$("$scriptRelativePath"/fileReader.sh "$scriptRelativePath"/sizes.txt)
-chmod +x ""$scriptRelativePath"/pictureSizeParser.py"
-parsedSizes=$(python "$scriptRelativePath"/pictureSizeParser.py $unparsedsizes)
-echo $parsedSizes
-
-#build sizes array
-IFS=',' read -ra sizes <<< "$parsedSizes"
-for i in "${sizes[@]}"; do
-    echo $i
-done
 #TODO:
 #separate pictures
 
