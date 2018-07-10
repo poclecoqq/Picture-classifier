489b43fcdd107760c9da369b276515d37f9aed59
Pierre-Olivier Cote <pierre-olivierb.cote@bnc.ca>
2018-07-09 21:59:53 -0400

    remove old directories before operating

--- pictureSeparator.sh
+++ pictureSeparator.sh
@@ -3,14 +3,19 @@
 
 scriptRelativePath=`dirname $0`
 
+oldDirectories=`find . -maxdepth 1 -type d -name "PS_*"`
+for dir in $oldDirectories; do
+  rm -rf $dir
+done
 
-files=`find . -maxdepth 1 -name "*.jpg" -o -name "*.png" -o -name "*.bmp" -o -name "*.gif"`
+
+files=`find . -maxdepth 1 -type f -name "*.jpg" -o -name "*.png" -o -name "*.bmp" -o -name "*.gif"`
 
 #for each picture in repository
 for file in $files; do
   size=$(python "$scriptRelativePath"/pictureSizeParser.py `file $file`)
   mkdir -p "PS_"$size""
-  cp $file "PS_"$size""
+  cp -n $file "PS_"$size""
 done
 
 echo "done"
