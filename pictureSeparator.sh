688a859a8a0731890697033edbc3f22cdbc2e463
Pierre-Olivier Cote <pierre-olivierb.cote@bnc.ca>
2018-07-11 14:07:59 -0400

    move or copy command line arg

--- pictureSeparator.sh
+++ pictureSeparator.sh
@@ -13,13 +13,19 @@ if $clean; then
     rm -rf $dir
   done
 else
+  if $move; then
+    pictureTranferCommand="mv"
+  else
+    pictureTranferCommand="cp -n" 
+  fi
+
   files=`find . -maxdepth 1 -type f -name "*.jpg" -o -name "*.png" -o -name "*.bmp" -o -name "*.gif"`
 
   #for each picture in repository
   for file in $files; do
     size=$(python "$scriptRelativePath"/pictureSizeParser.py `file $file`)
     mkdir -p "PS_"$size""
-    cp -n $file "PS_"$size""
+    eval $pictureTranferCommand $file "PS_"$size""
   done
 fi
 
