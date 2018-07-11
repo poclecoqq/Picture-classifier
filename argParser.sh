993a37eecf1e66d832944516eaefe94af3434b4e
Pierre-Olivier Cote <pierre-olivierb.cote@bnc.ca>
2018-07-11 14:00:37 -0400

    added argParser to read console line arg

--- argParser.sh
+++ argParser.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
+move=false
+clean=false
 
+for arg in "$@"; do
+  case $arg in 
+    -c|--clean)
+      clean=true
+    ;;
+    -m|--move)
+      move=true
+    ;;
+    *)
+      echo unknow parameter "$arg"
+  esac
+done
